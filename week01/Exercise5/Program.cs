66214d279d80c7b2d458f28f5043221f273348da
FerAmadorSolano <fernanda.ama25@gmail.com>
2026-06-29 21:32:18 -0600

    fix

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Hello World! This is the Exercise 5 Project.");
 
         DisplayWelcomeMessage();
 
