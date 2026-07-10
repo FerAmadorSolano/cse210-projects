ae56057f42419847f61f87966aef8195c4e6cd8b
FerAmadorSolano <fernanda.ama25@gmail.com>
2026-07-10 17:46:13 -0600

    first part

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public Journal()
+    {
+
+    }
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DysplayJournal()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadFromFile(string file)
+    {
+
+    }
+}
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Numerics;
+using System.Reflection.Emit;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
+        int option = 0;
+
+        //Ask the user for the option
+
+        do
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("What would you like to do?");
+            option = int.Parse(Console.ReadLine());
+        } while (option != 5);
     }
 }
