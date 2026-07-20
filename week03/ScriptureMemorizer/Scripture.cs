b8f4f0e19192cd1fd8edd6417b481f2f3d5abf26
FerAmadorSolano <fernanda.ama25@gmail.com>
2026-07-19 21:27:11 -0600

    first version

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] words = text.Split(' ');
+
+        foreach (string word in words)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        int hidden = 0;
+
+        while(hidden < numberToHide && !IsCompletelyHidden())
+        {
+            int index = _random.Next(_words.Count);
+            
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hidden++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = "";
+        foreach (Word word in _words)
+        {
+            text += word.GetDisplayText() + " ";
+        }
+
+        return $"{_reference.GetDisplayText()}\n\n{text}";
+    }
+    
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
